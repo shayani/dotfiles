867664f7b59b96119f7a28e68a8cdc2e37a9f63d
Fernando Shayani <shayani@gmail.com>
2024-07-26 16:24:14 -0300

    Only installs VSCode if has desktop

--- run_once_80-vscode.sh
+++ run_once_80-vscode.sh
@@ -2,6 +2,8 @@
 
 set -e
 
-wget -O /tmp/code.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
-sudo apt install -y /tmp/code.deb
-rm /tmp/code.deb
+if [ -n "${XDG_CURRENT_DESKTOP}" ]; then
+  wget -O /tmp/code.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
+  sudo apt install -y /tmp/code.deb
+  rm /tmp/code.deb
+fi
