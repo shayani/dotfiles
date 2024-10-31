5864cff69e8de6de32073a0d3ba36f97992cfcc4
Fernando Shayani <shayani@gmail.com>
2024-10-31 19:14:53 -0300

    Remove a instalação de programas

--- run_once_80-vscode.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/zsh
-
-set -e
-
-if [ -n "${XDG_CURRENT_DESKTOP}" ]; then
-  wget -O /tmp/code.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
-  sudo apt install -y /tmp/code.deb
-  rm /tmp/code.deb
-fi
--- run_once_900-gnome-wallpaper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ -n "${XDG_CURRENT_DESKTOP}" ]; then
-  /usr/bin/gsettings set org.gnome.desktop.background picture-uri file:////home/shayani/Pictures/wallpaper_meditacao.jpg  
-fi
-
