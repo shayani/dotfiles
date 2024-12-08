2e400be34a01754f366d4a5ab08fbacfbf15bf9e
Fernando Shayani <shayani@gmail.com>
2024-12-08 06:57:24 -0300

    Run once: APT

---
+++ run_once_10-apt.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y \
+  age \
+  bat \
+  btop \
+  build-essential \
+  curl \
+  fontconfig \
+  gcc \
+  git \
+  neofetch \
+  neovim \
+  procps \
+  ripgrep \
+  tldr \
+  tmux \
+  unzip \
+  wget \
+  zoxide \
+  zsh
