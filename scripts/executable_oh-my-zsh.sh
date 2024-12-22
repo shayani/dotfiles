82ad55f8dc2881e38f29efc9f01458d8ce2ecbfa
Fernando Shayani <shayani@gmail.com>
2024-12-22 07:22:56 -0300

    Adiciona script para instalar o Oh-my-zsh

---
+++ scripts/executable_oh-my-zsh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
