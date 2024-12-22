ce0e7e1c65a110ab2f0852f36c34505a7f552aee
Fernando Shayani <shayani@gmail.com>
2024-12-22 09:50:24 -0300

    Adiciona script para Homebrew

---
+++ scripts/executable_homebrew.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+brew install lazydocker gcc
