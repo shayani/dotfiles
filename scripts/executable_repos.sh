d5e759c153a7e3bc2b86edb59a3333ab10531c70
Fernando Shayani <shayani@gmail.com>
2024-12-27 15:17:42 -0300

    Adiciona script para clonar repos

---
+++ scripts/executable_repos.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd ~/devel/
+git clone git@github.com:Monoline-Corp/web-app.git monoline
+git clone git@github.com:deptinfo/snsa.git snsa
+git clone git@github.com:eventaservo/eventaservo.git eventaservo
+git clone git@github.com:shayani/kontilo.git kontilo
