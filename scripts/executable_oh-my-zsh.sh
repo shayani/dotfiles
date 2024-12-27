0de7cee5855ff5971102ec1af888235b15e8e4bc
Fernando Shayani <shayani@gmail.com>
2024-12-27 14:34:04 -0300

    Corrige script do Oh-my-zsh

--- scripts/executable_oh-my-zsh.sh
+++ scripts/executable_oh-my-zsh.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+rm -rf ~/.zshrc
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 
 
