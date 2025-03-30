4792b483380c1c00118b5e47ce78249c87755844
Fernando Shayani <shayani@gmail.com>
2025-03-30 07:31:57 -0300

    Atualiza scripts

--- scripts/executable_cedilla.sh
+++ scripts/executable_cedilla.sh
@@ -1,1 +1,10 @@
-echo "GTK_IM_MODULE=cedilla" >/etc/environment.d/50-cedilla.conf
+# Check if the OS is Arch Linux
+if [[ -f /etc/arch-release ]]; then
+  echo "Using Arch"
+  environment_file="/etc/environment"
+else
+  environment_file="/etc/environment.d/50-cedilla.conf"
+fi
+
+echo "GTK_IM_MODULE=cedilla" > "$environment_file"
+echo "QT_IM_MODULE=cedilla" >> "$environment_file"
