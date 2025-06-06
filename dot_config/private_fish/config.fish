c711d399f04e085afdc31b63bf4181003bb4a5c0
Fernando Shayani <shayani@gmail.com>
2025-06-06 10:57:45 -0300

    Fish

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    # Alias
+    alias ld='lazydocker'
+    alias lg='lazygit'
+
+    # Zoxide
+    zoxide init fish | source
+end
