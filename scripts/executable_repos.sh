58647e8f100649f09c805099b4b5b2b4707617c1
Fernando Shayani <shayani@gmail.com>
2025-06-14 06:23:17 -0300

    Adiciona Biskvito

--- scripts/executable_repos.sh
+++ scripts/executable_repos.sh
@@ -4,5 +4,5 @@ cd ~/devel/
 git clone git@github.com:Monoline-Corp/web-app.git monoline
 git clone git@github.com:deptinfo/snsa.git snsa
 git clone git@github.com:eventaservo/eventaservo.git eventaservo
-git clone git@github.com:shayani/molly.git molly
 git clone git@github.com:shayani/kontilo.git kontilo
+git clone git@github.com:biskvito/web_app.git ~/devel/biskvito/web_app
