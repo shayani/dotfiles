10f86e5361c21145343d233a4058f8591c2d87c6
Fernando Shayani <shayani@gmail.com>
2025-07-06 07:46:50 -0300

    Hyprland

--- run_once_12-hyprland.sh
+++ run_once_12-hyprland.sh
@@ -3,5 +3,16 @@
 # Check if the OS is Arch Linux
 if [[ -f /etc/arch-release ]]; then
   echo "Installing Hyprland dependencies on Arch Linux..."
-  paru -Syu hyprland wofi hyprpaper hypridle waybar
+  paru -Syu --needed \
+	  hyprland \
+	  wofi \
+	  hyprpaper \
+	  hypridle \
+	  waybar \
+	  hyprshot \
+	  swaync \
+	  xdg-desktop-portal-hyprland \
+	  hyprpolkitagent \
+	  matugen-bin
+
 fi
---
+++ scripts/executable_change_wallpaper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+WALLPAPER_DIR="/home/shayani/Pictures/wallpapers"
+CURRENT_WALLPAPER=$(hyprctl hyprpaper listloaded)
+
+# Get a random wallpaper from the directory
+NEW_WALLPAPER=$(ls $WALLPAPER_DIR | shuf -n 1)
+
+# Apply wallpaper
+hyprctl hyprpaper reload ,"$WALLPAPER_DIR/$NEW_WALLPAPER"
+matugen image "$WALLPAPER_DIR/$NEW_WALLPAPER"
+
