3126f75524157d1e51f0f2c81cd4f356be6cd818
Fernando Shayani <shayani@gmail.com>
2025-08-07 23:03:18 -0300

    Melhoramentos

--- run_once_10-arch-packages.sh
+++ run_once_10-arch-packages.sh
@@ -4,27 +4,32 @@
 if [[ -f /etc/arch-release ]]; then
   echo "Using Arch"
   paru -Syu --noconfirm --needed \
-	  btop \
-	  fd \
-	  fish \
-	  fontconfig \
-	  git \
-		impala \
-	  neovim \
-		slurp \
-		wl-screenrec \
-		wiremix \
-		satty \
-		swayosd \
-		ttf-cascadia-mono-nerd \
-	  tldr \
-	  tmux \
-	  unzip \
-	  wget \
-	  zsh \
-	  zsh \
+    btop \
+    fd \
+    fish \
+    fontconfig \
+    git \
+    impala \
+    neovim \
+    slurp \
+    wl-screenrec \
+    wiremix \
+    satty \
+    swayosd \
+    ttf-cascadia-mono-nerd \
+    tldr \
+    tmux \
+    unzip \
+    wget \
+    zsh \
+    zsh \
     ffmpegthumbnailer \
     mtr \
     ncdu \
+    walker-bin \
+    bluetui \
+    blueman \
+    lazygit \
+    lazydocker-bin \
     zoxide
 fi
