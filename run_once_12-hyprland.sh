074fb099404aa918d1817a2db6e5c921f7e4d2f2
Fernando Shayani <shayani@gmail.com>
2025-08-21 06:37:27 -0300

    feat(hyprland): Remove SwayNC and add wlogout

--- run_once_12-hyprland.sh
+++ run_once_12-hyprland.sh
@@ -3,17 +3,18 @@
 # Check if the OS is Arch Linux
 if [[ -f /etc/arch-release ]]; then
   echo "Installing Hyprland dependencies on Arch Linux..."
-  paru -Syu --needed --noconfirm \
-	  hyprland \
-	  wofi \
-	  hyprpaper \
-	  hypridle \
-	  waybar \
-	  hyprshot \
-	  hyprlock \
-	  mako \
-	  xdg-desktop-portal-hyprland \
-	  hyprpolkitagent \
-	  matugen-bin
+  paru -S --needed --noconfirm \
+    hyprland \
+    wofi \
+    hyprpaper \
+    hypridle \
+    waybar \
+    hyprshot \
+    hyprlock \
+    mako \
+    xdg-desktop-portal-hyprland \
+    hyprpolkitagent \
+    wlogout \
+    matugen-bin
 
 fi
