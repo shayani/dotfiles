8f417e6914527d7f4869418c529c39b263ee84ee
Fernando Shayani <shayani@gmail.com>
2025-09-17 20:47:02 -0300

    Remove grapite

--- run_once_13-aur-apps.sh
+++ run_once_13-aur-apps.sh
@@ -5,7 +5,6 @@ if [[ -f /etc/arch-release ]]; then
   echo "Instalando apps"
   paru -S --needed --noconfirm \
     brave-bin \
-    graphite-cli \
     opencode-bin \
     solaar \
     todoist-bin
