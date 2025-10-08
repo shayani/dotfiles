9dc43953a1e0fe12d1ed07b29875ddb7f8b8f867
Fernando Shayani <shayani@gmail.com>
2025-10-08 06:25:21 -0300

    Remove pastas que agora estão no sync

--- scripts/executable_repos.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd ~/devel/
-git clone git@github.com:Monoline-Corp/web-app.git monoline
-git clone git@github.com:deptinfo/snsa.git snsa
-git clone git@github.com:eventaservo/eventaservo.git eventaservo
-git clone git@github.com:shayani/kontilo.git kontilo
-git clone git@github.com:biskvito/web_app.git ~/devel/biskvito/web_app
--- scripts/executable_screenshot.sh
+++
@@ -1,9 +0,0 @@
-OUTPUT_DIR="$HOME/Pictures/Screenshots"
-
-pkill slurp || hyprshot -m ${1:-region} --raw |
-  satty --filename - \
-    --output-filename "$OUTPUT_DIR/screenshot-$(date +'%Y-%m-%d_%H-%M-%S').png" \
-    --early-exit \
-    --actions-on-enter save-to-clipboard \
-    --save-after-copy \
-    --copy-command 'wl-copy'
